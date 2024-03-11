84fcc40f5f0bc2f2bfb29b9c8c2aa4712f7dc574
iput2020-kkudo <it00036kosei.kudo@gmail.com>
2024-03-12 03:36:38 +0900

    メンバー紹介のページを追加

--- CodePrint.py
+++ CodePrint.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__, static_folder='.', static_url_path='')
 @app.route('/')
 def index():
-    return app.send_static_file('index.html')
+    return app.send_static_file('test.html')
 # Flask Server
 app.run(port=8000, debug=True)
 

c827705366b7847be5a0eb9fe2ea373cdffb1f1e
iput2020-kkudo <it00036kosei.kudo@gmail.com>
2024-03-12 15:45:52 +0900

    htmlをちょっと編集した

--- CodePrint.py
+++ CodePrint.py
@@ -1,10 +1,22 @@
-from flask import Flask
+from flask import Flask, Response
+import requests
 
 app = Flask(__name__, static_folder='.', static_url_path='')
+
 @app.route('/')
 def index():
-    return app.send_static_file('test.html')
-# Flask Server
-app.run(port=8000, debug=True)
+    url = 'https://raw.githubusercontent.com/Tenchi-desu/MemoryOfTakemotoLab/main/test.html'
+    response = requests.get(url)
+    if response.status_code == 200:
+        return Response(response.content, mimetype='text/html')
 
-# Hello
+@app.errorhandler(404)
+def page_not_found(error):
+    return Response("404 - Not Found Memories ...", status=404)
+
+@app.errorhandler(500)
+def page_not_found(error):
+    return Response("500 - Too many Memories To Process !!", status=500)
+
+if __name__ == '__main__':
+    app.run(port=8000, debug=False)
